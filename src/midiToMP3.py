b43922922c87ff9296da312e7520edc4dea694a4
ajuanijustus <ajuani1997@gmail.com>
2022-10-29 18:46:55 +0100

    Fix path join

--- src/midiToMP3.py
+++ src/midiToMP3.py
@@ -13,8 +13,7 @@ MIDI_FILE = "major-scale"
 
 # Gen path to file
 path = os.getcwd()
-path += "\\midiFiles\\"
-path += f"{MIDI_FILE}.mid"
+path = os.path.join(path, "midiFiles", f"{MIDI_FILE}.mid")
 
 # Convert midi to wav file
 fs.midi_to_audio(path, "output.wav")
