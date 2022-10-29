fe807458e4a6d082dff40b236bfd0b5fbee95588
awsloth <awsloth@gmail.com>
2022-10-30 00:57:25 +0100

    Added spooky mode

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,4 @@
 # Import from modules
-from turtle import width
 from generateMIDI import generate_midi
 from midiToMP3 import midi_to_mp3
 
@@ -7,6 +6,7 @@ from midiToMP3 import midi_to_mp3
 import pygame
 import os
 import sys
+import shutil
 
 # Button Class
 class Button:
@@ -48,6 +48,7 @@ class Button:
         self.width = self.content.get_width()
         self.height = self.content.get_height()
 
+# Input Class
 class IntInput(Button):
     def __init__(self, x, y, text, width, border=5):
         super().__init__(x, y, text, border)
@@ -74,7 +75,6 @@ class IntInput(Button):
         if not self.typing:
             self.inner_colour = 255
         return False
-
 
 # Function to generte new song
 def gen_new(base_note, scale_type, chords_flag):
@@ -121,6 +121,8 @@ mp3_filename = os.path.join(path, "mp3Files", f"{MIDI_FILENAME}.mp3")
 pygame.mixer.init()
 pygame.font.init()
 
+cat = pygame.image.load(os.path.join(path, "images", "catPump.jpg"))
+
 # Load the song
 pygame.mixer.music.load(mp3_filename)
 
@@ -137,15 +139,20 @@ opp = {"Chords On":"Chords Off", "Chords Off":"Chords On"}
 # Buttons
 buttonGen = Button(10, 10, "Re-Generate")
 buttonChords = Button(80, 10, "Chords Off")
+buttonSave = Button(260, 10, "Save")
 intInput = IntInput(140, 10, " ", 100)
-
 
 # Var to keep track of music
 play = False
+spooky_keys = [pygame.K_s, pygame.K_p, pygame.K_k, pygame.K_y]
+spooky_mode = False
 
 # Main interface loop
 while True:
     pressed_keys = []
+    k = pygame.key.get_pressed()
+    spooky = [k[i] for i in spooky_keys]
+    mouse_pressed = False
 
     # Manage each event
     for event in pygame.event.get():
@@ -159,12 +166,17 @@ while True:
             elif event.key == pygame.K_BACKSPACE:
                 pressed_keys.append("back")
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pressed = True
+
+
     # Clear the screen
     screen.fill((255, 255, 255))
 
     # Draw buttons
     buttonGen.draw(screen)
     buttonChords.draw(screen)
+    buttonSave.draw(screen)
     intInput.draw(screen)
 
     # Check if first button hovered/clicked
@@ -172,16 +184,19 @@ while True:
         if intInput.text != " ":
             base_note = int(intInput.text)
         gen_new(base_note, "major", chords_flag)
-        intInput.text = " "
-        intInput.rerender()
         play = False
 
     # Check if second button hovered/clicked
-    if buttonChords.hovered(*pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+    if buttonChords.hovered(*pygame.mouse.get_pos()) and mouse_pressed:
         buttonChords.text = opp[buttonChords.text]
         buttonChords.rerender()
         chords_flag = not chords_flag
 
+    if buttonSave.hovered(*pygame.mouse.get_pos()) and mouse_pressed:
+        mp3_path = os.path.join(path, "mp3Files")
+        files = os.listdir(mp3_path)
+        shutil.copy(mp3_filename, os.path.join(mp3_path, f"song{len(files)-1}.mp3"))
+        
     if intInput.hovered(*pygame.mouse.get_pos()):
         if pygame.mouse.get_pressed()[0]:
             intInput.typing = True
@@ -207,6 +222,21 @@ while True:
     if not play:
         pygame.mixer.music.play()
         play = True
+
+    if sum(spooky) == 4:
+        spooky_mode = True
+    
+    if spooky_mode:
+        cat_x = 293+cat.get_width()//2
+        cat_y = 18+cat.get_height()//2
+        cat_wid = 15
+        cat_hight = 8
+        mx, my = pygame.mouse.get_pos()
+        if cat_x <= mx <= cat_x+cat_wid and cat_y <= my <= cat_y+cat_hight and mouse_pressed:
+            # extra_spooky_gen
+            spooky_mode = False
+            print("boo!")
+        screen.blit(cat, (300, 10))
     
     # Update the screen
     pygame.display.flip()
