79abf98781ef546f2a2d6c881bc3ddc67291d48b
awsloth <awsloth@gmail.com>
2022-10-29 15:22:49 +0100

    Created code to convert from midi to mp3

--- src/midiToMP3.py
+++ src/midiToMP3.py
@@ -1,2 +1,24 @@
-from midi2audio import FluidSynth
+# Import libraries
+import os
 
+# Import from libraries
+from midi2audio import FluidSynth
+from pydub import AudioSegment
+
+# Instantiate fluid synth
+fs = FluidSynth()
+
+# Constant name of file
+MIDI_FILE = "bohemian"
+
+# Gen path to file
+path = os.getcwd()
+path += "\\midiFiles\\"
+path += f"{MIDI_FILE}.mid"
+
+# Convert midi to wav file
+fs.midi_to_audio(path, "output.wav")
+
+# Open wav file and convert to mp3
+song = AudioSegment.from_wav("output.wav")
+song.export(f"{MIDI_FILE}.mp3", format="mp3")
